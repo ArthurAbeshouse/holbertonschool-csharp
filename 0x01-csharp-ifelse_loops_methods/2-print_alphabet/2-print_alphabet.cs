010cc3a5c43ee43d0727573779b94927069e8990
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-14 19:12:49 -0400

    prints the alphabet in lowercase

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char lower_alpha = 'a'; lower_alpha <= 'z'; lower_alpha++)
+        Console.Write(lower_alpha);
+    }
+}
