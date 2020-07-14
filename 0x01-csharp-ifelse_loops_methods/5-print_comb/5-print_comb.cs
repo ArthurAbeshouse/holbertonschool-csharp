5906070707e55a891d4983432057fce2772f445c
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-14 19:49:02 -0400

    added new line when program hits 99

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -13,7 +13,7 @@ namespace _5_print_comb
                 if (i != 99)
                 Console.Write(", ");
             }
-            Console.WriteLine();
+            Console.Write("\n");
         }
     }
 }
