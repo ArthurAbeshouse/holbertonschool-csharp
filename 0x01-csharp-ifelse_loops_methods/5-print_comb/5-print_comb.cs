35343a61fb138acc0bd13795c64f8c3a1cc9e532
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-14 19:39:14 -0400

    prints 0 to 99, with commas

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 99; i++)
+            {
+                Console.Write("{0}", i);
+
+                if (i != 99)
+                Console.Write(", ");
+            }
+        }
+    }
+}
