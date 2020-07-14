5c1a8509bfa98186ad928339ea9ae30f4b8fb586
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-14 19:19:05 -0400

    prints the alphabet in lowercase, without 'q' and 'e'

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char lower_alpha = 'a'; lower_alpha <= 'z'; lower_alpha++)
+            {
+                if (lower_alpha != 'q' && lower_alpha != 'e')
+                Console.Write(lower_alpha);
+            }
+        }
+    }
+}
