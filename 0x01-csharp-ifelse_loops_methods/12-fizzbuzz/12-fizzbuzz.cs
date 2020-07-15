ec757b24c5603ede16479dacd49d08247e7120cd
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-14 20:34:34 -0400

    Fizz Buzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int fizz = 1; fizz <= 100; fizz++)
+            {
+                if (fizz % 3 == 0 && fizz % 5 == 0)
+                Console.Write("FizzBuzz");
+                else if (fizz % 3 == 0)
+                Console.Write("Fizz");
+                else if (fizz % 5 == 0)
+                Console.Write("Buzz");
+                else
+                Console.Write("{0}", fizz);
+                if (fizz < 100)
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
