f686d6fbdf5c5e9f0db7b5147ff2438a0b36c6cd
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-14 20:25:43 -0400

    fixed output

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -10,10 +10,10 @@ namespace _6_print_comb2
             {
                 for (int n2 = 1; n2 <= 9; n2++)
                 {
-                    if (n2  > n1)
-                    Console.Write("{0}{1}, ", n1, n2);
                     if (n1 == 8 && n2 == 9)
                     Console.WriteLine("{0}{1}", n1, n2);
+                    else if (n1 < n2)
+                    Console.Write("{0}{1}, ", n1, n2);
                 }
             }
         }
