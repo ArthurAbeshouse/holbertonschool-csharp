be61e657b0a04e86380e1da2d1297930478185fe
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-16 14:38:40 -0400

    Print an array of integers

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int [] array_of_nums = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array_of_nums[i] = i;
+            if (i > 0)
+                Console.Write(" ");
+            Console.Write(i);
+        }
+        Console.WriteLine();
+        return array_of_nums;
+    }
+}
