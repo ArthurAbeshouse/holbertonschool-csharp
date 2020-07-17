56ca7dd48c6095e3793cc56d5512ef6ce25d0764
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-16 21:03:28 -0400

    returns the key with the biggest integer value in a given dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string results = "None";
+        int value = 0;
+        foreach(KeyValuePair<string, int> KeyVP in myList)
+        {
+            if (value <= KeyVP.Value)
+            {
+                value = KeyVP.Value;
+                results = KeyVP.Key;
+            }
+        }
+        return results;
+    }
+}
