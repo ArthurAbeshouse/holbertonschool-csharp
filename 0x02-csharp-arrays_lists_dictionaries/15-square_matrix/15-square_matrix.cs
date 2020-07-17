806e243c604566fbc92c3450ebd1ff318513177d
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-16 21:51:50 -0400

    computes the square value of all integers of a matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int row = myMatrix.GetLength(0);
+        int column = myMatrix.GetLength(1);
+        int[,] NewMatrix = new int[row, column];
+        for (int a = 0; a < row; ++a)
+        {
+            for (int b = 0; b < column; ++b)
+            {
+                NewMatrix[a, b] = myMatrix[a, b] * myMatrix[a, b];
+            }
+        }
+        return NewMatrix;
+    }
+}
