b09f170e266240e9e62bed8038c82502001fc1dd
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-27 21:47:33 -0400

    Print a LinkedList

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+       LinkedList<int> new_list = new LinkedList<int>();
+
+       for (int n = 0; n < size; ++n)
+       {
+           new_list.AddLast(n);
+           Console.WriteLine(n);
+       }
+
+       return new_list;
+    }
+}
