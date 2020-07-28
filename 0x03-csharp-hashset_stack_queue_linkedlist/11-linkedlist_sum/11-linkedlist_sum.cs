a30704c441fafe3c497cba89fdfb6b49fea925aa
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-27 22:22:18 -0400

    LinkedList sum

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int result = 0;
+        foreach (int n in myLList)
+            result += n;
+        return result;
+    }
+}
