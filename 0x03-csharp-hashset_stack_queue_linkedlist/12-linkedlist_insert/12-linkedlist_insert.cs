652eef1e0ed0fe8d47d17eca6f642c4d4374bb76
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-27 22:33:47 -0400

    Insert at index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> new_Node = new LinkedListNode<int>(n);
+        LinkedListNode<int> current = myLList.First;
+
+        if (myLList == null)
+        {
+            myLList.AddFirst(new_Node);
+            return new_Node;
+        }
+        if (current.Value > n)
+        {
+            myLList.AddBefore(current, new_Node);
+            return new_Node;
+        }
+        while (current.Next != null)
+        {
+            if (current.Next.Value >= n)
+                break;
+            current = current.Next;
+        }
+        myLList.AddAfter(current, new_Node);
+        return new_Node;
+    }
+}
