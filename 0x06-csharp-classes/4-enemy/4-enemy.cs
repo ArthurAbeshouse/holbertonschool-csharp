d76fb9028631fc5ccdec697df90b2725af742774
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-30 19:20:07 -0400

    Hello, my name is

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents a Zombie</summary>
+    public class Zombie
+    {
+        ///<summary>Health field of Zombie</summary>
+        private int health;
+        ///<summary>Zombie name</summary>
+        private string name = "(No name)";
+
+        ///<summary>Constructor for <see cref="Zombie"/> class</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        ///<summary>Constructor for <see cref="Zombie"/> class with argument</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArithmeticException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+
+        ///<summary>Gets the Zombie's health</summary>
+        ///<return>The Zombie's health</return>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        /// <summary>
+        /// Getter and setter for Zombie name
+        /// </summary>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+    }
+}
