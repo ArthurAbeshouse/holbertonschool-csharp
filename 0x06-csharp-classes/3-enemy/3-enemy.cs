ea694dc747d574ba2565a6e2cb6ead7c3931a736
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-30 19:27:51 -0400

    Zombie health

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents a Zombie</summary>
+    public class Zombie
+    {
+        ///<summary>Health field of Zombie</summary>
+        private int health;
+
+        ///<summary>Constructor for <see cref="Zombie"/> class</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        ///<summary>Constructor for <see cref="Zombie"/> class with argument</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArithmeticException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+
+        ///<summary>Gets the Zombie's health</summary>
+        ///<return>The Zombie's health</return>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
