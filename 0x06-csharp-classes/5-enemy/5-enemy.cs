6a0cf53f60d367902d72ec2c841037d51ddbc7e0
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-07-30 19:26:51 -0400

    Printing a zombie

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents a Zombie</summary>
+    public class Zombie
+    {
+        ///<summary>Health field of Zombie</summary>
+        private int health;
+        ///<summary>Zombie name</summary>
+        private string name = "(No name)";
+
+        ///<summary>Constructor for <see cref="Zombie"/> class</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        ///<summary>Constructor for <see cref="Zombie"/> class with argument</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArithmeticException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+
+        ///<summary>Gets the Zombie's health</summary>
+        ///<returns>The Zombie's health</returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        /// <summary>
+        /// Getter and setter for Zombie name
+        /// </summary>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        ///<summary>Override the default ToString method</summary>
+        ///<returns>String of Zombie's attributes</returns>
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
+
+    }
+}
