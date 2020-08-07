57f3c0df682061f6516b470b1b54821ba91bca5d
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-06 20:56:07 -0400

    Only subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// method of IsInstanceOfArray
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Returns True if the object is an instance 
+    /// of a class that inherits from the specified class
+    /// </summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+            return true;
+        return false;
+    }
+}
