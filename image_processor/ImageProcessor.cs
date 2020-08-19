b8ffacb7ff4901cf1622da3d45e6c28f3b6acc0a
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-19 17:15:30 -0400

    Grayscale

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -50,7 +50,33 @@ class ImageProcessor
     /// <param name="filenames"></param>
     public static void Grayscale(string[] filenames)
     {
+        Parallel.ForEach(filenames, img_name =>
+        {
+            Bitmap image = new Bitmap(img_name);
+            Rectangle rekt = new Rectangle(0, 0, image.Width, image.Height);
+            BitmapData bmpdata = image.LockBits(rekt, ImageLockMode.ReadWrite, image.PixelFormat);
 
+            IntPtr ptr = bmpdata.Scan0;
+
+            int bytes = Math.Abs(bmpdata.Stride) * image.Height;
+            byte[] rgbValues = new byte[bytes];
+
+            Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            for (int i = 0; i < rgbValues.Length - 2; i += 3)
+            {
+                byte grayscale = (byte)(rgbValues[i] * 0.21 + 0.71 * rgbValues[i + 1] + 0.071 * rgbValues[i + 2]);
+                rgbValues[i] = rgbValues[i + 1] = rgbValues[i + 2] = grayscale;
+            }
+            
+            Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+            image.UnlockBits(bmpdata);
+
+            string file_name = Path.GetFileNameWithoutExtension(img_name);
+            string file_extension = Path.GetExtension(img_name);
+            image.Save($"{file_name}_inverse{file_extension}");
+        });
     }
 
     /// <summary>
