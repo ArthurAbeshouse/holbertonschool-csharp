8a0ca86852c03042b390732e311ce48e48832bc8
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-25 16:08:56 -0400

    fixed spacing

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -14,7 +14,7 @@ class MatrixMath
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
         double[,] err = { { -1 } };
-       if ((matrix1.GetLength(0) < 2 && matrix1.GetLength(0) > 3 && matrix1.GetLength(1) < 2 && matrix1.GetLength(1) > 3) ||
+        if ((matrix1.GetLength(0) < 2 && matrix1.GetLength(0) > 3 && matrix1.GetLength(1) < 2 && matrix1.GetLength(1) > 3) ||
         (matrix2.GetLength(0) < 2 && matrix2.GetLength(0) > 3 && matrix2.GetLength(1) < 2 && matrix2.GetLength(1) > 3))
             return err;
         if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
