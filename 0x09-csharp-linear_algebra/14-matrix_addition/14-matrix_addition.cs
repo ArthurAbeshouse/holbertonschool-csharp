bbb30987d73b4b02c130704cf161753490c67d1e
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-25 15:13:06 -0400

    Matrix addition #1

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -6,13 +6,24 @@
 class MatrixMath
 {
     /// <summary>
-    /// 
+    /// Adds two matrices
     /// </summary>
     /// <param name="matrix1"></param>
     /// <param name="matrix2"></param>
-    /// <returns></returns>
+    /// <returns>The results, else -1 if not a 2D or 3D matrix</returns>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
+        if ((matrix1.GetLength(0) < 2 && matrix1.GetLength(1) < 2 && matrix2.GetLength(0) < 2 && matrix2.GetLength(1) < 2) ||
+        (matrix1.GetLength(0) > 3 && matrix1.GetLength(1) > 3 && matrix2.GetLength(0) > 3 && matrix2.GetLength(1) > 3))
+            return new double[,] { { -1 } };
         
+        double[,] results = new double[matrix1.Length, matrix1.Length];
+        int i, j;
+        for (i = 0; i < matrix1.GetLength(0); i += 1)
+        {
+            for (j = 0; j < matrix1.GetLength(0); j += 1)
+                results[i, j] = matrix1[i, j] + matrix2[i, j];
+        }
+        return results;
     }
 }
