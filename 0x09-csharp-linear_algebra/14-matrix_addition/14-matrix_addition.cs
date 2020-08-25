68b6b6d41eecbe3f8be7023bcd03a367c06afb8f
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-25 16:02:23 -0400

    fixed 2D and 3D matrix check

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -10,11 +10,11 @@ class MatrixMath
     /// </summary>
     /// <param name="matrix1"></param>
     /// <param name="matrix2"></param>
-    /// <returns>The results, else -1 if not a 2D or 3D matrix</returns>
+    /// <returns>The results, else -1 if not a 2D or 3D matrix or both matrices aren't the same size</returns>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        if ((matrix1.GetLength(0) < 2 && matrix1.GetLength(1) < 2 && matrix2.GetLength(0) < 2 && matrix2.GetLength(1) < 2) ||
-        (matrix1.GetLength(0) > 3 && matrix1.GetLength(1) > 3 && matrix2.GetLength(0) > 3 && matrix2.GetLength(1) > 3))
+        if (matrix1.GetLength(0) < 2 && matrix1.GetLength(0) > 3 && matrix1.GetLength(1) < 2 && matrix1.GetLength(1) > 3 &&
+        matrix2.GetLength(0) < 2 && matrix2.GetLength(0) > 3 && matrix2.GetLength(1) < 2 && matrix2.GetLength(1) > 3)
             return new double[,] { { -1 } };
         if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
             return new double[,] { { -1 } };
