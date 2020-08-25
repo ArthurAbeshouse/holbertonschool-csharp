948bd3bfc0a4f4e3d0713181af90c2dcd3eb6baa
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-25 15:57:22 -0400

    checks if both matrices are not the same size

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -13,8 +13,10 @@ class MatrixMath
     /// <returns>The results, else -1 if not a 2D or 3D matrix</returns>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        if ((matrix1.GetLength(0) < 2 && matrix1.GetLength(1) < 2 && matrix2.GetLength(0) < 2 && matrix2.GetLength(1) < 2) &&
+        if ((matrix1.GetLength(0) < 2 && matrix1.GetLength(1) < 2 && matrix2.GetLength(0) < 2 && matrix2.GetLength(1) < 2) ||
         (matrix1.GetLength(0) > 3 && matrix1.GetLength(1) > 3 && matrix2.GetLength(0) > 3 && matrix2.GetLength(1) > 3))
+            return new double[,] { { -1 } };
+        if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
             return new double[,] { { -1 } };
         
         double[,] results = new double[matrix1.GetLength(0), matrix1.GetLength(0)];
