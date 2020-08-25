7e0ce13308b3edfb6ae6b4b6e1cacbbc2532512b
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-25 15:45:04 -0400

    seperate checks for 2D and 3D matrices

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -13,14 +13,15 @@ class MatrixMath
     /// <returns>The results, else -1 if not a 2D or 3D matrix</returns>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        if ((matrix1.GetLength(0) < 2 && matrix1.GetLength(1) < 2 && matrix2.GetLength(0) < 2 && matrix2.GetLength(1) < 2) ||
-        (matrix1.GetLength(0) > 3 && matrix1.GetLength(1) > 3 && matrix2.GetLength(0) > 3 && matrix2.GetLength(1) > 3))
+        if (matrix1.GetLength(0) < 2 && matrix1.GetLength(1) < 2 && matrix2.GetLength(0) < 2 && matrix2.GetLength(1) < 2)
             return new double[,] { { -1 } };
-        
+        else if (matrix1.GetLength(0) > 3 && matrix1.GetLength(1) > 3 && matrix2.GetLength(0) > 3 && matrix2.GetLength(1) > 3)
+            return new double[,] { { -1 } };
         double[,] results = new double[matrix1.GetLength(0), matrix1.GetLength(0)];
-        for (int i = 0; i < matrix1.GetLength(0); i += 1)
+        int i, j;
+        for (i = 0; i < matrix1.GetLength(0); i += 1)
         {
-            for (int j = 0; j < matrix1.GetLength(1); j += 1)
+            for (j = 0; j < matrix1.GetLength(1); j += 1)
                 results[i, j] = matrix1[i, j] + matrix2[i, j];
         }
         return results;
