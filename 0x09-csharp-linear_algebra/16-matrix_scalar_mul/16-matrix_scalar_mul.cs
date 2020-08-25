f0780f8c2aa2bdf305ce84c95c5d8bbd8ba7f79e
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-25 16:45:16 -0400

    Matrix-scalar multiplication #1

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace _16_matrix_scalar_mul
+/// <summary>
+/// Class for MatrixMath
+/// </summary>
+class MatrixMath
 {
-    class Program
+    /// <summary>
+    /// Multiplies a matrix and a scalar
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <param name="scalar"></param>
+    /// <returns>The results, else -1 if not a 2D or 3D matrix</returns>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        static void Main(string[] args)
+        if ((matrix.GetLength(0) != 2 && matrix.GetLength(0) != 3) || (matrix.GetLength(1) != 2 && matrix.GetLength(1) != 3))
+            return new double[,] { { -1 } };
+
+        int i, j;
+        for (i = 0; i < matrix.GetLength(0); i += 1)
         {
-            Console.WriteLine("Hello World!");
+            for (j = 0; j < matrix.GetLength(1); j += 1)
+                matrix[i, j] *= scalar;
         }
+        return matrix;
     }
 }
