63c45f6a09317f17a8c211b677d3538cfa74b3df
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-25 12:10:28 -0400

    fixed 6

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -25,7 +25,7 @@ class VectorMath
             double[] Vector3 = { 0, 0, 0 };
             Vector3[0] = vector1[0] + vector2[0];
             Vector3[1] = vector1[1] + vector2[1];
-            Vector3[1] = vector1[2] + vector2[2];
+            Vector3[2] = vector1[2] + vector2[2];
             return Vector3;
         }
         return new double[] { -1 };
