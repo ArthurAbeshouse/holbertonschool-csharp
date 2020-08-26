3fe75f77100362927b037dcac0e747699ad0a3b2
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-26 16:58:15 -0400

    Fixed output and added comments

--- 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -14,25 +14,32 @@ class MatrixMath
     /// <returns></returns>
     public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
-        if ((matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2) || (direction != 'x' && direction != 'y'))
+        if (matrix.GetLength(0) != 2 && matrix.GetLength(1) != 2)
             return new double[,] { { -1 } };
 
         double[,] newMatrix = new double[2, 2];
 
-        if (direction == 'x')
+        if (direction == 'x' || direction == 'y')
         {
-            newMatrix[0, 0] = matrix[0, 0] + factor * matrix[0, 1];
-            newMatrix[0, 1] = matrix[0, 1];
-            newMatrix[1, 0] = matrix[1, 0] + factor * matrix[1, 1];
-            newMatrix[1, 1] = matrix[1, 1];
+            if (direction == 'x')
+            {
+                newMatrix[0, 0] = matrix[0, 0] + factor * matrix[0, 1];
+                newMatrix[0, 1] = matrix[0, 1];
+                newMatrix[1, 0] = matrix[1, 0] + factor * matrix[1, 1];
+                newMatrix[1, 1] = matrix[1, 1];
+
+                return newMatrix;
+            }
+            else
+            {
+                newMatrix[0, 0] = matrix[0, 0];
+                newMatrix[0, 1] = matrix[0, 1] * factor + matrix[0, 0];
+                newMatrix[1, 0] = matrix[1, 0];
+                newMatrix[1, 1] = matrix[1, 1] * factor + matrix[1, 0];
+
+                return newMatrix;
+            }
         }
-        else
-        {
-            newMatrix[0, 0] = matrix[0, 0];
-            newMatrix[0, 1] = matrix[0, 1] * factor + matrix[0, 0];
-            newMatrix[1, 0] = matrix[1, 0];
-            newMatrix[1, 1] = matrix[1, 1] * factor + matrix[1, 0];
-        }
-        return newMatrix;
+        return new double[,] { { -1 } };
     }
 }
