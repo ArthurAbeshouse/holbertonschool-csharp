24cc150beaa3ba4e7acb5b4ac95083a5175349ee
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-25 22:26:39 -0400

    Cross product #1

--- 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace _30_cross_product
+/// <summary>
+/// Class for VectorMath
+/// </summary>
+class VectorMath
 {
-    class Program
+    /// <summary>
+    /// Calculates the cross product of two 3D vectors
+    /// </summary>
+    /// <param name="vector1"></param>
+    /// <param name="vector2"></param>
+    /// <returns>If either vector is not a 3D vector, it returns a vector containing -1</returns>
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
-        static void Main(string[] args)
+        if (vector1.Length == 3 && vector2.Length == 3)
         {
-            Console.WriteLine("Hello World!");
+            double[] results = new double[vector1.Length];
+            results[0] = (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]);
+            results[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]);
+            results[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
+            return results;
         }
+        return new double[] { -1 };
     }
 }
