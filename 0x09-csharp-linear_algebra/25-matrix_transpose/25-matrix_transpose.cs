e8f376c9538e88efd07532107a04271a79ed70c3
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-26 18:03:31 -0400

    Transpose matrix

--- 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace _25_matrix_transpose
+/// <summary>
+/// Class for MatrixMath
+/// </summary>
+class MatrixMath
 {
-    class Program
+    /// <summary>
+    /// Transposes a matrix
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <returns>If the matrix is empty, it returns an empty matrix</returns>
+    public static double[,] Transpose(double[,] matrix)
     {
-        static void Main(string[] args)
+        int matrix_row = matrix.GetLength(0);
+        int matrix_column = matrix.GetLength(1);
+
+        if (matrix == null || matrix_row == 0 || matrix_column == 0)
+            return new double[,]{};
+        
+        double[,] result = new double[matrix_column, matrix_row];
+
+        for (int i = 0; i < matrix_row; i += 1)
         {
-            Console.WriteLine("Hello World!");
+            for (int j = 0; j < matrix_column; j += 1)
+                result[j, i] = matrix[i, j];
         }
+        return result;
     }
 }
