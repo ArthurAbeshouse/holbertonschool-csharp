fba68ce2ddcd6e5b720de217476247eb8cf56f0a
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-26 16:52:10 -0400

    Matrix shear #1

--- 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
-namespace _24_matrix_shear_2D
+/// <summary>
+/// Class for MatrixMath
+/// </summary>
+class MatrixMath
 {
-    class Program
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <param name="direction"></param>
+    /// <param name="factor"></param>
+    /// <returns></returns>
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
-        static void Main(string[] args)
+        if ((matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2) || (direction != 'x' && direction != 'y'))
+            return new double[,] { { -1 } };
+
+        double[,] newMatrix = new double[2, 2];
+
+        if (direction == 'x')
         {
-            Console.WriteLine("Hello World!");
+            newMatrix[0, 0] = matrix[0, 0] + factor * matrix[0, 1];
+            newMatrix[0, 1] = matrix[0, 1];
+            newMatrix[1, 0] = matrix[1, 0] + factor * matrix[1, 1];
+            newMatrix[1, 1] = matrix[1, 1];
         }
+        else
+        {
+            newMatrix[0, 0] = matrix[0, 0];
+            newMatrix[0, 1] = matrix[0, 1] * factor + matrix[0, 0];
+            newMatrix[1, 0] = matrix[1, 0];
+            newMatrix[1, 1] = matrix[1, 1] * factor + matrix[1, 0];
+        }
+        return newMatrix;
     }
 }
